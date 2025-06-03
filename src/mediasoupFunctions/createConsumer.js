560d47a21ff945cdaf146fb6507faf164a23c9f5
ObiomaIkpe <obiomaikpe1@gmail.com>
2025-06-03 14:23:11 +0100

    consume process continued

--- src/mediasoupFunctions/createConsumer.js
+++ src/mediasoupFunctions/createConsumer.js
@@ -1,6 +1,6 @@
 
 
-const createConsumer = (consumerTransport, pid, device, socket, type, slot) => {
+const createConsumer = (consumerTransport, pid, device, socket, kind, slot) => {
     return new Promise(async(resolve, reject) => {
 
         const consumerParams = await socket.emitWithAck('consumeMedia', {       rtpCapabilities: device.rtpCapabilities,
